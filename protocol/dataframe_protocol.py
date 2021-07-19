3796eb2c11e0acc3b72d998386e6d7c48c36306a
Athan Reines <kgryte@gmail.com>
2021-07-19 11:25:58 -0700

    Fix copy

--- protocol/dataframe_protocol.py
+++ protocol/dataframe_protocol.py
@@ -308,20 +308,20 @@ class Column:
 
         The returned dictionary has the following contents:
 
-            - "data": a two-element tuple whose first element is a tuple
-                      containing the buffer containing the data and whose second
-                      element is the data buffer's associated dtype.
-            - "validity": a two-element tuple whose first element is a tuple
-                          containing the buffer containing mask values
-                          indicating missing data and whose second element is
-                          the mask value buffer's associated dtype. None if the
-                          null representation is not a bit or byte mask.
-            - "offsets": a two-element tuple whose first element is a tuple
-                         containing the buffer containing the offset values for
-                         variable-size binary data (e.g., variable-length
-                         strings) and whose second element is the offsets
-                         buffer's associated dtype. None if the data buffer does
-                         not have an associated offsets buffer.
+            - "data": a two-element tuple whose first element is a buffer
+                      containing the data and whose second element is the data
+                      buffer's associated dtype.
+            - "validity": a two-element tuple whose first element is a buffer
+                          containing mask values indicating missing data and
+                          whose second element is the mask value buffer's
+                          associated dtype. None if the null representation is
+                          not a bit or byte mask.
+            - "offsets": a two-element tuple whose first element is a buffer
+                         containing the offset values for variable-size binary
+                         data (e.g., variable-length strings) and whose second
+                         element is the offsets buffer's associated dtype. None
+                         if the data buffer does not have an associated offsets
+                         buffer.
         """
         pass
 
--- protocol/pandas_implementation.py
+++ protocol/pandas_implementation.py
@@ -509,20 +509,20 @@ class _PandasColumn:
 
         The returned dictionary has the following contents:
 
-            - "data": a two-element tuple whose first element is a tuple
-                      containing the buffer containing the data and whose second
-                      element is the data buffer's associated dtype.
-            - "validity": a two-element tuple whose first element is a tuple
-                          containing the buffer containing mask values
-                          indicating missing data and whose second element is
-                          the mask value buffer's associated dtype. None if the
-                          null representation is not a bit or byte mask.
-            - "offsets": a two-element tuple whose first element is a tuple
-                         containing the buffer containing the offset values for
-                         variable-size binary data (e.g., variable-length
-                         strings) and whose second element is the offsets
-                         buffer's associated dtype. None if the data buffer does
-                         not have an associated offsets buffer.
+            - "data": a two-element tuple whose first element is a buffer
+                      containing the data and whose second element is the data
+                      buffer's associated dtype.
+            - "validity": a two-element tuple whose first element is a buffer
+                          containing mask values indicating missing data and
+                          whose second element is the mask value buffer's
+                          associated dtype. None if the null representation is
+                          not a bit or byte mask.
+            - "offsets": a two-element tuple whose first element is a buffer
+                         containing the offset values for variable-size binary
+                         data (e.g., variable-length strings) and whose second
+                         element is the offsets buffer's associated dtype. None
+                         if the data buffer does not have an associated offsets
+                         buffer.
         """
         buffers = {}
         buffers["data"] = self._get_data_buffer()
