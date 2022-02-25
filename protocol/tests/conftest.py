87231e0ca1e168a7829315edd5a0544d89839788
Maria Rubtsova <maria.rubtsova@intel.com>
2022-02-26 00:36:34 +0300

    change conftest

--- protocol/tests/conftest.py
+++ protocol/tests/conftest.py
@@ -1,7 +1,20 @@
 import pytest
 import pandas as pd
+from pandas.api.exchange.implementation import _from_dataframe
 
 
-@pytest.fixture
-def constructor_frame(data):
-    return pd.DataFrame(data)
+@pytest.fixture(scope="package")
+def df_from_dict():
+    def maker(dct, is_categorical=False):
+        df = pd.DataFrame(dct)
+        return df.astype("category") if is_categorical else df
+
+    return maker
+
+
+@pytest.fixture(scope="package")
+def df_from_xchg():
+    def maker(xchg):
+        return _from_dataframe(xchg)
+
+    return maker
