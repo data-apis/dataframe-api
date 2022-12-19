98b0c5f217be5dd148ea273c402227dae9570a20
Brock <jbrockmendel@gmail.com>
2022-12-19 14:00:14 -0800

    init python package for API spec

---
+++ spec/API_specification/dataframe_api/__init__.py
@@ -0,0 +1,9 @@
+"""
+Function stubs and API documentation for the DataFrame API standard.
+"""
+
+__dataframe_api_version__: str = "YYYY.MM"
+"""
+String representing the version of the DataFrame API specification to which the
+conforming implementation adheres.
+"""
