dba7bbfa75884bf5ebdc4c03a2e54b241e049ed2
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-04-13 17:15:46 +0100

    add `any` and `all` rowwise (#127)

--- spec/API_specification/dataframe_api/dataframe_object.py
+++ spec/API_specification/dataframe_api/dataframe_object.py
@@ -501,6 +501,34 @@ class DataFrame:
             If any of the DataFrame's columns is not boolean.
         """
         ...
+    
+    def any_rowwise(self, skipna: bool = True) -> Column:
+        """
+        Reduction returns a Column.
+
+        Differs from ``DataFrame.any`` and that the reduction happens
+        for each row, rather than for each column.
+
+        Raises
+        ------
+        ValueError
+            If any of the DataFrame's columns is not boolean.
+        """
+        ...
+
+    def all_rowwise(self, skipna: bool = True) -> Column:
+        """
+        Reduction returns a Column.
+
+        Differs from ``DataFrame.all`` and that the reduction happens
+        for each row, rather than for each column.
+
+        Raises
+        ------
+        ValueError
+            If any of the DataFrame's columns is not boolean.
+        """
+        ...
 
     def min(self, skipna: bool = True) -> DataFrame:
         """
