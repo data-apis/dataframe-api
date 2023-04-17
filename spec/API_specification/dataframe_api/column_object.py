e689f0be6d7267afee341007093b39e0ef4adbd4
MarcoGorelli <>
2023-04-17 09:43:22 +0100

    add __len__ and __getitem__ to Column

--- spec/API_specification/dataframe_api/column_object.py
+++ spec/API_specification/dataframe_api/column_object.py
@@ -1,2 +1,24 @@
+from typing import NoReturn
+
 class Column:
-    pass
+    def __len__(self) -> int:
+        """
+        Return the number of rows.
+        """
+    
+    def __getitem__(self, row) -> object:
+        """
+        Get the element at row index `row`.
+        """
+
+    def __iter__(self) -> NoReturn:
+        """
+        Iterate over elements.
+
+        This is intentionally "poisoned" to discourage inefficient code patterns.
+
+        Raises
+        ------
+        NotImplementedError
+        """
+        raise NotImplementedError("'__iter__' is intentionally not implemented.")
