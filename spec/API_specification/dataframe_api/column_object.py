27a2853f69dcfaa67d6b0e5bab7f97135c971cf9
MarcoGorelli <>
2023-04-19 11:17:20 +0100

    add from_sequence

--- spec/API_specification/dataframe_api/column_object.py
+++ spec/API_specification/dataframe_api/column_object.py
@@ -1,2 +1,23 @@
+from __future__ import annotations
+
+from typing import Sequence
+
+from ._types import dtype
+
 class Column:
-    pass
+    @classmethod
+    def from_sequence(cls, sequence: Sequence[object], dtype: dtype) -> Column:
+        """
+        Construct Column from sequence of elements.
+
+        Parameters
+        ----------
+        sequence : Sequence[object]
+            Sequence of elements.
+        dtype : str
+            Dtype of result. Must be specified.
+        
+        Returns
+        -------
+        Column
+        """
