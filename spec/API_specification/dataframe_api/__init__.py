533b904924982404b8deae65d9d73f3ec8272f69
MarcoGorelli <>
2023-05-04 17:50:28 +0100

    move from_sequence to namespace

--- spec/API_specification/dataframe_api/__init__.py
+++ spec/API_specification/dataframe_api/__init__.py
@@ -1,10 +1,15 @@
 """
 Function stubs and API documentation for the DataFrame API standard.
 """
+from __future__ import annotations
+
+from typing import Sequence
 
 from .column_object import *
 from .dataframe_object import *
 from .groupby_object import *
+
+from ._types import dtype
 
 
 __dataframe_api_version__: str = "YYYY.MM"
@@ -12,3 +17,22 @@ __dataframe_api_version__: str = "YYYY.MM"
 String representing the version of the DataFrame API specification to which the
 conforming implementation adheres.
 """
+
+def from_sequence(sequence: Sequence[object], dtype: dtype) -> Column:
+    """
+    Construct Column from sequence of elements.
+
+    Parameters
+    ----------
+    sequence : Sequence[object]
+        Sequence of elements. Each element must be of the specified
+        ``dtype``, the corresponding Python builtin scalar type, or
+        coercible to that Python scalar type.
+    dtype : str
+        Dtype of result. Must be specified.
+
+    Returns
+    -------
+    Column
+    """
+    ...
--- spec/API_specification/dataframe_api/column_object.py
+++ spec/API_specification/dataframe_api/column_object.py
@@ -17,26 +17,6 @@ class Column:
     constructor functions or an already-created dataframe object retrieved via
 
     """
-    @classmethod
-    def from_sequence(cls, sequence: Sequence[object], dtype: dtype) -> Column:
-        """
-        Construct Column from sequence of elements.
-
-        Parameters
-        ----------
-        sequence : Sequence[object]
-            Sequence of elements. Each element must be of the specified
-            ``dtype``, the corresponding Python builtin scalar type, or
-            coercible to that Python scalar type.
-        dtype : str
-            Dtype of result. Must be specified.
-        
-        Returns
-        -------
-        Column
-        """
-        ...
-
     def __len__(self) -> int:
         """
         Return the number of rows.
