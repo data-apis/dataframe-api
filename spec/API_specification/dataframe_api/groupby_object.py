6a8a428bfc591bba52480ef466820edd3ca955fd
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-07-07 13:33:24 +0100

    fixup

--- spec/API_specification/dataframe_api/groupby_object.py
+++ spec/API_specification/dataframe_api/groupby_object.py
@@ -43,17 +43,10 @@ class GroupBy:
     def mean(self, *, skip_nulls: bool = True) -> DataFrame:
         ...
 
-<<<<<<< HEAD
-    def std(self, *, skip_nulls: bool = True) -> DataFrame:
+    def std(self, *, correction: int | float = 1, skip_nulls: bool = True) -> DataFrame:
         ...
 
-    def var(self, *, skip_nulls: bool = True) -> DataFrame:
-=======
-    def std(self, *, correction: int | float = 1, skip_nulls: bool = True) -> "DataFrame":
-        ...
-
-    def var(self, *, correction: int | float = 1, skip_nulls: bool = True) -> "DataFrame":
->>>>>>> upstream/main
+    def var(self, *, correction: int | float = 1, skip_nulls: bool = True) -> DataFrame:
         ...
 
     def size(self) -> DataFrame:
