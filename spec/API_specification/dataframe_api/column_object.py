3ef3bcbacd7668f5cc50dce5ea548dc5fc1450cd
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-08-01 14:57:41 +0100

    nitpicky

--- spec/API_specification/dataframe_api/column_object.py
+++ spec/API_specification/dataframe_api/column_object.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any,NoReturn, Sequence, TYPE_CHECKING, Literal, Generic
+from typing import Any,NoReturn, TYPE_CHECKING, Literal, Generic
 
 from ._types import DType
 
