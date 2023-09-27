2a2a96a984aa7dc22b7e646eb4f87883ad23f454
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-09-27 16:56:43 +0100

    exclude examples from conf.py

--- spec/conf.py
+++ spec/conf.py
@@ -113,7 +113,12 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+    'API_specification/examples/*',
+]
 
 # MyST options
 myst_heading_anchors = 3
