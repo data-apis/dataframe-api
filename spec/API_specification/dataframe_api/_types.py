280808cfdcab472427c96f97f7637a6da7d51051
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-09-28 14:16:56 +0100

    fixup

--- spec/API_specification/dataframe_api/_types.py
+++ spec/API_specification/dataframe_api/_types.py
@@ -37,23 +37,6 @@ if TYPE_CHECKING:
 
     DType = Union[Bool, Float64, Float32, Int64, Int32, Int16, Int8, UInt64, UInt32, UInt16, UInt8]
 
-if TYPE_CHECKING:
-    from .dtypes import (
-        Bool,
-        Float64,
-        Float32,
-        Int64,
-        Int32,
-        Int16,
-        Int8,
-        UInt64,
-        UInt32,
-        UInt16,
-        UInt8,
-    )
-
-    DType = Union[Bool, Float64, Float32, Int64, Int32, Int16, Int8, UInt64, UInt32, UInt16, UInt8]
-
 # Type alias: Mypy needs Any, but for readability we need to make clear this
 # is a Python scalar (i.e., an instance of `bool`, `int`, `float`, `str`, etc.)
 Scalar = Any
