a449943c54955b6eefd516f5e04fe0ccf9a9bc0b
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-21 13:51:18 +0100

    add original query

--- spec/API_specification/examples/tpch/q5.py
+++ spec/API_specification/examples/tpch/q5.py
@@ -1,6 +1,23 @@
+"""
+Original query:
+
+SELECT n_name, SUM(l_extendedprice * (1 - l_discount)) AS revenue
+  FROM customer, orders, lineitem, supplier, nation, region
+ WHERE c_custkey = o_custkey
+   AND l_orderkey = o_orderkey
+   AND l_suppkey = s_suppkey
+   AND c_nationkey = s_nationkey
+   AND s_nationkey = n_nationkey
+   AND n_regionkey = r_regionkey
+   AND r_name = 'ASIA'
+   AND o_orderdate >= MDY(1,1,1994)
+   AND o_orderdate < MDY(1,1,1994) + 1 UNITS YEAR
+GROUP BY n_name
+ORDER BY revenue DESC
+"""
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from dataframe_api.typing import SupportsDataFrameAPI
