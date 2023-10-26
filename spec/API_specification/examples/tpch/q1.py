5112b1244290c517a8b2fac2fd4ba7eb6d2affd1
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-26 10:45:11 +0100

    fixup;

--- spec/API_specification/examples/tpch/q1.py
+++ spec/API_specification/examples/tpch/q1.py
@@ -8,23 +8,21 @@ def query(lineitem_raw: SupportsDataFrameAPI) -> Any:
     lineitem = lineitem_raw.__dataframe_consortium_standard__()
     namespace = lineitem.__dataframe_namespace__()
 
-    mask = lineitem.get_column_by_name("l_shipdate") <= namespace.date(1998, 9, 2)  # type: ignore
+    mask = lineitem.get_column_by_name("l_shipdate") <= namespace.date(1998, 9, 2)
     lineitem = lineitem.assign(
-        [
-            (
-                lineitem.get_column_by_name("l_extended_price")
-                * (1 - lineitem.get_column_by_name("l_discount"))
-            ).rename("l_disc_price"),
-            (
-                lineitem.get_column_by_name("l_extended_price")
-                * (1 - lineitem.get_column_by_name("l_discount"))
-                * (1 + lineitem.get_column_by_name("l_tax"))
-            ).rename("l_charge"),
-        ]
+        (
+            lineitem.get_column_by_name("l_extended_price")
+            * (1 - lineitem.get_column_by_name("l_discount"))
+        ).rename("l_disc_price"),
+        (
+            lineitem.get_column_by_name("l_extended_price")
+            * (1 - lineitem.get_column_by_name("l_discount"))
+            * (1 + lineitem.get_column_by_name("l_tax"))
+        ).rename("l_charge"),
     )
     result = (
         lineitem.filter(mask)
-        .group_by(["l_returnflag", "l_linestatus"])
+        .group_by("l_returnflag", "l_linestatus")
         .aggregate(
             namespace.Aggregation.sum("l_quantity").rename("sum_qty"),
             namespace.Aggregation.sum("l_extendedprice").rename("sum_base_price"),
@@ -34,6 +32,6 @@ def query(lineitem_raw: SupportsDataFrameAPI) -> Any:
             namespace.Aggregation.mean("l_discount").rename("avg_disc"),
             namespace.Aggregation.size().rename("count_order"),
         )
-        .sort(["l_returnflag", "l_linestatus"])
+        .sort("l_returnflag", "l_linestatus")
     )
     return result.dataframe
