b879f31ac596451aa2986d82fd0c934758e1f84d
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-11-15 12:32:54 +0000

    add Scalar.dtype and Scalar.persist

--- spec/API_specification/dataframe_api/scalar_object.py
+++ spec/API_specification/dataframe_api/scalar_object.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
-from typing import Any, Protocol
+from typing import TYPE_CHECKING, Protocol
+
+if TYPE_CHECKING:
+    from dataframe_api.typing import AnyScalar, DType
 
 __all__ = ["Scalar"]
 
@@ -13,64 +16,66 @@ class Scalar(Protocol):
     as `:meth:`Column.sum`.
     """
 
-    def __lt__(self, other: Any) -> Scalar:
+    def __lt__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __le__(self, other: Any) -> Scalar:
+    def __le__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __eq__(self, other: object) -> Scalar:  # type: ignore[override]
+    def __eq__(self, other: AnyScalar) -> Scalar:  # type: ignore[override]
         ...
 
-    def __ne__(self, other: object) -> Scalar:  # type: ignore[override]
+    def __ne__(self, other: AnyScalar) -> Scalar:  # type: ignore[override]
         ...
 
-    def __gt__(self, other: Any) -> Scalar:
+    def __gt__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __ge__(self, other: Any) -> Scalar:
+    def __ge__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __add__(self, other: Any) -> Scalar:
+    def __add__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __radd__(self, other: Any) -> Scalar:
+    def __radd__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __sub__(self, other: Any) -> Scalar:
+    def __sub__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __rsub__(self, other: Any) -> Scalar:
+    def __rsub__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __mul__(self, other: Any) -> Scalar:
+    def __mul__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __rmul__(self, other: Any) -> Scalar:
+    def __rmul__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __mod__(self, other: Any) -> Scalar:
+    def __mod__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __rmod__(self, other: Any) -> Scalar:
+    # Signatures of "__rmod__" of "Scalar" and "__mod__" of "str | int | float | Scalar"
+    # are unsafely overlapping
+    def __rmod__(self, other: AnyScalar) -> Scalar:  # type: ignore[misc]
         ...
 
-    def __pow__(self, other: Any) -> Scalar:
+    def __pow__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __rpow__(self, other: Any) -> Scalar:
+    def __rpow__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __floordiv__(self, other: Any) -> Scalar:
+    def __floordiv__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __rfloordiv__(self, other: Any) -> Scalar:
+    def __rfloordiv__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __truediv__(self, other: Any) -> Scalar:
+    def __truediv__(self, other: AnyScalar) -> Scalar:
         ...
 
-    def __rtruediv__(self, other: Any) -> Scalar:
+    def __rtruediv__(self, other: AnyScalar) -> Scalar:
         ...
 
     def __neg__(self) -> Scalar:
@@ -83,5 +88,23 @@ class Scalar(Protocol):
         """Note that this return a Python scalar.
 
         Depending on the implementation, this may raise or trigger computation.
+        """
+        ...
+    
+    @property
+    def dtype(self) -> DType:
+        """Return data type of column."""
+        ...
+
+    def persist(self) -> object:
+        """Hint that computation prior to this point should not be repeated.
+
+        This is intended as a hint, rather than as a directive. Implementations
+        which do not separate lazy vs eager execution may ignore this method and
+        treat it as a no-op.
+
+        .. note::
+            This may trigger computation and so should be used with care.
+            See `execution_model` page for more details.
         """
         ...
--- spec/API_specification/examples/05_scalars_example.py
+++ spec/API_specification/examples/05_scalars_example.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from dataframe_api.column_object import Column
+    from dataframe_api.dataframe_object import DataFrame
+    from dataframe_api.scalar_object import Scalar
     from dataframe_api.typing import SupportsDataFrameAPI
 
 
@@ -16,4 +19,16 @@ def main(df_raw: SupportsDataFrameAPI) -> SupportsDataFrameAPI:
     # ... but also Python scalars:
     df = df.fill_null(3)
     df = df.fill_null("3")
+
+    # Scalars can be used in arithmetic expressions with other scalars, columns,
+    # or DataFrames
+    value: Scalar = df.col("a").mean()
+    col: Column = df.col("a")
+    _res1: Column = value - col
+    _res2: Scalar = value - 3
+    _res3: Scalar = 3 - value
+    _res4: Column = df.col("a") - 3
+    _res5: DataFrame = df - value
+    _res6: DataFrame = value - df
+
     return df.dataframe
