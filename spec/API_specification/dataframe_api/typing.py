61adbc327effd95e6c5c9b0dad821b6ae4f86bce
Marco Edward Gorelli <marcogorelli@protonmail.com>
2023-11-21 11:05:08 +0000

    typing (#330)

--- spec/API_specification/dataframe_api/typing.py
+++ spec/API_specification/dataframe_api/typing.py
@@ -69,6 +69,9 @@ class Namespace(Protocol):
     null: NullType
 
     class Datetime:
+        time_unit: Literal['ms', 'us']
+        time_zone: str | None
+
         def __init__(  # noqa: ANN204
             self,
             time_unit: Literal["ms", "us"],
@@ -77,6 +80,8 @@ class Namespace(Protocol):
             ...
 
     class Duration:
+        time_unit: Literal['ms', 'us']
+
         def __init__(  # noqa: ANN204
             self,
             time_unit: Literal["ms", "us"],
