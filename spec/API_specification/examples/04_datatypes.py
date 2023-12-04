c20db2d3f0ed6dd63288366ca07fbaf797a030fd
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-12-04 15:45:00 +0000

    lint

--- spec/API_specification/examples/04_datatypes.py
+++ spec/API_specification/examples/04_datatypes.py
@@ -12,11 +12,7 @@ def main(df_raw: SupportsDataFrameAPI) -> SupportsDataFrameAPI:
     df = df_raw.__dataframe_consortium_standard__(api_version="2023-11.beta").persist()
     pdx = df.__dataframe_namespace__()
     df = df.select(
-        *[
-            col.name
-            for col in df.iter_columns()
-            if isinstance(col.dtype, pdx.Int64)
-        ],
+        *[col.name for col in df.iter_columns() if isinstance(col.dtype, pdx.Int64)],
     )
     arr = df.to_array()
     arr = some_array_function(arr)
