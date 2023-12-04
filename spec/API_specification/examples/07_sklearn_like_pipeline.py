c87ebf4baef5a15754a600d2a7f0062809db6954
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-12-04 16:05:45 +0000

    update

--- spec/API_specification/examples/07_sklearn_like_pipeline.py
+++ spec/API_specification/examples/07_sklearn_like_pipeline.py
@@ -1,4 +1,5 @@
-"""
+"""Scikit-learn-like pipeline.
+
 This is an example of how a (possibly) lazy data frame may be used
 in a sklearn-like pipeline.
 
@@ -8,34 +9,36 @@ eager. They compute some state that is later meant to be transferred
 into the ONNX graph/model. That transfer happens when calling
 `transform`. The logic within the `transform` methods is "traced"
 lazily and the resulting lazy object is then exported to ONNX.
-""" 
+"""
 from __future__ import annotations
 
-from typing import Any, TYPE_CHECKING, Self
+from typing import TYPE_CHECKING, Any
 
-from dataframe_api.dataframe_object import DataFrame
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+    from dataframe_api.typing import Column, DataFrame, Scalar
 
 
 class Scaler:
-    """Apply a standardization scaling factor to `column_names`."""    
-    scalings_: dict[str, float]
+    """Apply a standardization scaling factor to `column_names`."""
 
-    def __init__(self, column_names: list[str]):
+    scalings_: dict[str, Scalar]
+
+    def __init__(self, column_names: list[str]) -> None:
         self.column_names = column_names
 
     def fit(self, df: DataFrame) -> Self:
         """Compute scaling factors from given data frame.
 
-        Calling this function requires collecting values.
+        A typical data science workflow is to fit on one dataset,
+        and then transform and multiple datasets. Therefore, we
+        make sure to `persist` within the `fit` method.
         """
-        scalings = df.select(self.column_names).std()
-        if hasattr(scalings, 'collect'):
-            scalings = scalings.collect()
+        scalings = df.select(*self.column_names).std().persist()
 
         self.scalings_ = {
-            # Note: `get_value` returns an implemenation-defined,
-            # duck-typed scalar which may be lazy.
-            column_name: scalings.get_column_by_name(column_name).get_value(0)
+            column_name: scalings.col(column_name).get_value(0)
             for column_name in self.column_names
         }
 
@@ -46,14 +49,16 @@ class Scaler:
 
         This function is guaranteed to not collect values.
         """
+        columns: list[Column] = []
         for column_name in df.column_names:
-            if not column_name in self.column_names:
+            if column_name not in self.column_names:
                 continue
-            column = df.get_column_by_name(column_name) / self.scalings_[column_name]
-            # Note: `assign` is not in-place
-            df = df.assign(column)
+            column = df.col(column_name) / self.scalings_[column_name]
+            columns.append(column)
 
-        return df
+        # Note: `assign` is not in-place
+        return df.assign(*columns)
+
 
 class FeatureSelector:
     """Limit columns to those seen in training including their order."""
@@ -72,15 +77,15 @@ class FeatureSelector:
         This function is guaranteed to not collect values.
         """
         # Note: This assumes that select ensures the column order.
-        return df.select(self.columns_)
+        return df.select(*self.columns_)
 
 
 class Pipeline:
     """Linear pipeline of transformers."""
 
-    def __init__(self, steps: list[Any]):
+    def __init__(self, steps: list[Any]) -> None:
         self.steps = steps
-    
+
     def fit(self, df: DataFrame) -> Self:
         """Call fit on the steps of the pipeline subsequently.
 
@@ -101,4 +106,3 @@ class Pipeline:
             df = step.transform(df)
 
         return df
-
